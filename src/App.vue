14f53d740ceb8916e1a950a2decd4ef9d39d8668
Leon Czajka <leonpiotrczajka@gmail.com>
2021-09-08 23:22:18 +0200

    added a lot of features

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <!-- APPBAR -->
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>TODO</v-toolbar-title>
       <v-spacer></v-spacer>
@@ -7,98 +8,152 @@
       <v-btn text rounded>Login</v-btn>
     </v-app-bar>
 
-    <!-- Login module -->
     <v-main>
+      <!-- CONTAINER WITH TASKS -->
       <v-container>
         <v-row no-gutters>
+          <!-- TASK CARD -->
           <v-col
-            v-for="task in tasks"
-            :key="task.id"
+            v-for="(task, index) in tasks"
+            :key="index"
             cols="12"
             sm="4"
           >
-            <v-card class="pa-2 ma-2">
-              <v-card-title>
-                {{ task.title }}
-                <v-spacer></v-spacer>
-                <v-menu
-                  ref="menu"
-                  v-model="menu2"
-                  transition="scale-transition"
-                  :close-on-content-click="false"
-                  offset-y
-                  max-width="290px"
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn 
-                      text 
-                      v-on="on"
-                      v-bind="attrs"
-                    >
-                      {{ task.time }}
-                    </v-btn>  
-                  </template>
-                  <v-time-picker
-                    v-if="menu2"
-                    v-model="timeTmp"
-                    full-width
-                    @click:minute="updateTaskTime(task.id, timeTmp)"
-                  ></v-time-picker>
-                </v-menu>
-              </v-card-title>
-              <v-card-text>
-                <div>
-                  {{ task.desc }}
-                </div>
-              </v-card-text>
-            </v-card>
+            <v-hover v-slot="{ hover }">
+              <v-card 
+                :elevation="hover ? 12 : 2"
+                class="pa-2 ma-2"
+              >
+                <v-card-title>
+                  <div 
+                    v-if="!titleChange"
+                    @click="titleChange = true"
+                    class="card-title"
+                  >
+                    {{ task.title }}
+                  </div>
+                  <v-text-field
+                    v-else
+                    v-model="task.title"
+                    @blur="titleChange = false"
+                  ></v-text-field>
+                  <v-spacer></v-spacer>
+                  <!-- TIME WITH TIME PICKER MENU ON CLICK --> 
+                  <v-menu
+                    ref="menu"
+                    v-model="menu2"
+                    transition="scale-transition"
+                    :close-on-content-click="false"
+                    offset-y
+                    max-width="290px"
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn 
+                        text 
+                        v-on="on"
+                        v-bind="attrs"
+                      >
+                        {{ task.time }}
+                      </v-btn>  
+                    </template>
+                    <v-time-picker
+                      v-if="menu2"
+                      v-model="timeTmp"
+                      full-width
+                      @click:minute="updateTaskTime(index, timeTmp)"
+                    ></v-time-picker>
+                  </v-menu>
+                </v-card-title>
+
+                <v-card-text>
+                  <div
+                    v-if="!descChange"
+                    @click="descChange = true"
+                  >
+                    {{ task.desc }}
+                  </div>
+                  <v-textarea 
+                    v-model="task.desc"
+                    v-else
+                    @blur="descChange = false"
+                  ></v-textarea>
+                </v-card-text>
+                
+                <v-card-actions>
+                  <v-btn
+                    v-show="hover"
+                    icon
+                    @click="deleteTask(index)"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    v-show="hover"
+                    icon
+                    @click="archiveTask(index)"
+                  >
+                    <v-icon>mdi-check</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-hover>
           </v-col>
         </v-row>
       </v-container>
+
+      <!-- NEW TASK OVERLAY -->
       <v-overlay
-        :absolute="absolute"
         :value="overlay"
         z-index="0"
       >
-      <v-card
-        center
-        class="pa-2"
-      >
-        <v-card-title>Nowe zadanie</v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field 
-              v-model="taskTitle"
-              :error-messages="taskTitleErrors"
-              label="Tytuł"
-              required
-              @input="$v.taskTitle.$touch()"
-              @blur="$v.taskTitle.$touch()"
-            ></v-text-field>
-            <v-textarea 
-              v-model="taskDesc"
-              label="Opis" 
-              required
-              scrollable
-            ></v-textarea>
-          </v-form>
-          <v-time-picker
-            v-model="taskTime"
-            value="08:11"
-            format="24hr"
-          ></v-time-picker>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            @click="saveTask()"
-          >dodaj</v-btn>
-        </v-card-actions>
-      </v-card>
+        <v-card
+          center
+          class="pa-2"
+        >
+          <v-card-title>Nowe zadanie</v-card-title>
+
+          <v-card-text>
+            <v-form>
+              <v-text-field 
+                v-model="taskTitle"
+                :error-messages="taskTitleErrors"
+                label="Tytuł"
+                required
+                @input="$v.taskTitle.$touch()"
+                @blur="$v.taskTitle.$touch()"
+              ></v-text-field>
+              <v-textarea 
+                v-model="taskDesc"
+                label="Opis" 
+                required
+                scrollable
+              ></v-textarea>
+            </v-form>
+            <v-time-picker
+              v-model="taskTime"
+              value="08:11"
+              format="24hr"
+            ></v-time-picker>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              text
+              @click="cancelTaskAdding()"
+            >anuluj</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              @click="saveTask()"
+            >dodaj</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-overlay>
     </v-main>
+
+    <!-- NEW TASK FLOATING BUTTON -->
     <v-btn
       color="pink"
       fab
@@ -109,6 +164,14 @@
     >
       <v-icon>mdi-plus</v-icon>
     </v-btn>
+
+    <!-- FOOTER WITH NEXT TASK -->
+    <v-footer
+      color="primary lighten-1 white--text"
+    >
+      <div class="ma-4">Następne zadanie: {{ nextTask }}</div>
+    </v-footer>
+
   </v-app>
 </template>
 
@@ -123,11 +186,26 @@ export default {
   mixins: [validationMixin],
 
   computed: {
-    taskTitleErrors () {
+    taskTitleErrors() {
       const errors = []
         if (!this.$v.taskTitle.$dirty) return errors
         !this.$v.taskTitle.required && errors.push('Pole wymagane')
       return errors
+    },
+    nextTask() {
+      const currentMinutes = new Date().getHours() * 60 + new Date().getMinutes()
+      console.log("current minutes: " + currentMinutes)
+      var index
+      var tmpMinutes = 100000
+      for (var i = 0; i < this.tasks.length; i++) {
+        const taskMinutes = parseInt(this.tasks[i].time.substr(0, 2)) * 60 + parseInt(this.tasks[i].time.substr(3, 2))
+        if (taskMinutes - currentMinutes < tmpMinutes) {
+          index = i
+          tmpMinutes = taskMinutes - currentMinutes
+          console.log("task " + this.tasks[i].title + " minutes: " + taskMinutes + " tmpMinutes: " + tmpMinutes)
+        }
+      }
+      return this.tasks[index].title
     }
   },
 
@@ -136,36 +214,46 @@ export default {
   },
 
   data: () => ({
-    showPassword: false,
-    absolute: true,
     overlay: false,
     menu2: false,
     timeTmp: null,
+
+    titleChange: false,
+    descChange: false,
+
     taskTitle: '',
     taskDesc: '',
     taskTime: new Date().getHours() + ":" + new Date().getMinutes(),
     tasks: [{
-      id: 1,
-      time: '10:45',
+      time: '23:59',
       title: 'Naglowek',
       desc: 'To jest piekny opis'
-    }]
+    }],
+    doneTasks: [],
   }),
   methods: {
     saveTask(){
       this.$v.$touch()
       if (!this.$v.$invalid) {
-        this.tasks.push({id: this.tasks.at(-1).id + 1, time: this.taskTime, title: this.taskTitle, desc: this.taskDesc})
+        this.tasks.push({time: this.taskTime, title: this.taskTitle, desc: this.taskDesc})
         this.overlay = false
+        this.taskTitle = ''
+        this.taskDesc = ''
       }
     },
-    updateTaskTime(id, time) {
-      for (var i = 0; i < this.tasks.length; i++) {
-        console.log(i)
-        if (this.tasks[i].id == id) {
-          this.tasks[i].time = time
-        }
-      }
+    cancelTaskAdding() {
+      this.overlay = false
+      this.taskTitle = ''
+      this.taskDesc = ''
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    },
+    archiveTask(index) {
+      this.doneTasks.push(this.tasks.splice(index, 1))
+    },
+    updateTaskTime(index, time) {
+      this.tasks[index].time = time
       this.menu2 = false
     },
   }
@@ -173,7 +261,7 @@ export default {
 </script>
 
 <style scoped>
-/* .v-btn--floating {
-  position: absolute;
-} */
+.card-title {
+  width: 50%;
+}
 </style>
