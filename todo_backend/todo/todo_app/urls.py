faa15c524ce5f2786eba8129f26607976a44fa58
Leon Czajka <leonpiotrczajka@gmail.com>
2021-09-13 23:48:17 +0200

    added basic login funcionality

--- todo_backend/todo/todo_app/urls.py
+++ todo_backend/todo/todo_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import appView, putCsrfToken
+from .views import appView, putCsrfToken, loginUser
 
 urlpatterns = [
     path('', appView),
     path('login/', appView),
-    path('csrf/', putCsrfToken)
+    path('csrf/', putCsrfToken),
+    path('login/login/', loginUser),
 ]
--- todo_backend/todo/todo_app/views.py
+++ todo_backend/todo/todo_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.middleware import csrf
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -10,3 +11,13 @@ def appView(request):
 def putCsrfToken(request):
     token = csrf.get_token(request)
     return JsonResponse({'token': token})
+
+def loginUser(request):
+    if request.method == 'POST':
+        print(request.POST.get('username'))
+        print(request.POST.get('password'))
+
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        login(request, user)
+
+    return JsonResponse({'status': 'Success'})
